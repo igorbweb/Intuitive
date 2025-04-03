13f68e762105ee4c48d75e35d25c296615c16008
Igor <igorb@alfamaweb.com.br>
2025-04-03 20:27:44 -0300

    Vue.js

---
+++ 5. Vue/vue-intuitive/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <h1>Intuitive Care</h1>
+    <p>Componente para consumir a API de Operadoras</p>
+  </main>
+</template>
