dcb2955b3c3a8f533ae490512d913a6aa8c4c61d
Igor <igorb@alfamaweb.com.br>
2025-04-03 14:04:34 -0300

    Correção HTTP + arquivo no diretório

--- 4API/main.py
+++ 4API/main.py
@@ -1,28 +1,22 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 import pandas as pd
 from typing import List
 import os
 
 app = FastAPI()
 
-path = os.getcwd()
-
-CSV_PATH = os.path.join(path, "3. SQL", "Relatorio_cadop.csv")
-
-if os.path.exists(CSV_PATH):
-    df = pd.read_csv(CSV_PATH, delimiter=";", dtype=str)
-else:
-    df = pd.DataFrame()
-
+CSV_PATH = os.path.join(os.path.dirname(__file__), "Relatorio_cadop.csv")
 @app.get("/")
 def home():
-    return "Interactive Care"
+    try:
+        df = pd.read_csv(CSV_PATH, delimiter=";", dtype=str)
 
-@app.get("/operadoras/")
-async def buscar_operadoras(q: str = Query(..., min_length=3)):
-    """Busca operadoras pelo Nome Fantasia, Razão Social ou CNPJ."""
-    if df.empty:
-        return {"error": "Nenhum dado carregado"}
+        df = df.where(pd.notna(df), None)
 
-    resultados = df[df.apply(lambda row: row.astype(str).str.contains(q, case=False, na=False).any(), axis=1)]
-    return resultados.to_dict(orient="records")
+        if df.empty:
+            return {"message": "Nenhum dado encontrado no CSV"}
+        
+        return df.to_dict(orient="records")
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao processar CSV: {str(e)}")
